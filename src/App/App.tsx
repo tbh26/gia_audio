f8754fd0fc8cb2649830e927c1ac4b4dbdc3e501
Tjeerd Hes <tjeerd.hes@amsteldiep.nl>
2022-06-29 17:40:07 +0200

    [setup] bootstrapped with *create-react-app*

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <section className="App">
+      <header className="App-header">
+        <h2>graphql in action, audio</h2>
+      </header>
+    </section>
+  );
+}
+
+export default App;
